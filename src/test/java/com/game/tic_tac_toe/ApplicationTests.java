3779d2f5d60df443d56496d4b6c876b7b0e76916
arturzdan <arturzdan@mail.ru>
2022-01-29 00:09:13 +0300

    initial commit

---
+++ src/test/java/com/game/tic_tac_toe/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.game.tic_tac_toe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
